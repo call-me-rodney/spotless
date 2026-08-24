c9e9b38ed545450b91247c7626ce109a17edb338
call-me-rodney <kwmrodney@gmail.com>
2026-08-24 17:30:43 +0300

    Creating application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CaseModule } from './case/case.module';
+import { CollectorsModule } from './collectors/collectors.module';
+import { RoutingModule } from './routing/routing.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { WasteModule } from './waste/waste.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CaseModule, CollectorsModule, RoutingModule, AnalyticsModule, WasteModule],
   controllers: [AppController],
   providers: [AppService],
 })
